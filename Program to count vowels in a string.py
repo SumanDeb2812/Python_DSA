c606c597431e583fce63044da0a684c3aec53f82
SumanDeb2812 <sumandeb2812@gmail.com>
2024-03-02 23:24:44 +0530

    slove new dsa

---
+++ Program to count vowels in a string.py
@@ -0,0 +1,16 @@
+# Given a string, count the total number of vowels (a, e, i, o, u) in it. There are two methods to count total 
+# number of vowels in a string. 
+
+#using loop
+# def checkVowels(string):
+#     count = 0
+#     vowels = ['a', 'e', 'i', 'o', 'u']
+#     for i in string:
+#         for j in vowels:
+#             if i == j:
+#                 count += 1
+#     print(count)
+
+# checkVowels("aeiou")
+
+# recursive
